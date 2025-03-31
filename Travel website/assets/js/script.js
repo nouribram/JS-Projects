25dde8100ce6d1a87e7dfd13f81ea7ddbe2b0f4c
nouribram <nourtesla@yahoo.com>
2025-03-31 16:30:31 +0200

    new update

---
+++ Travel website/assets/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Navbar toggle */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+    this.classList.toggle("active");
+    header.classList.toggle("active");
+});
