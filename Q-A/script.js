59575378877f7873de2a60567975a63b39aa035e
nouribram <nourtesla@yahoo.com>
2025-05-24 22:15:31 +0300

    final

--- Q-A/script.js
+++ Q-A/script.js
@@ -37,6 +37,9 @@ function startQuiz() {
 }
 
 function showQuestion() {
+
+    resetState();
+
     let currentQuestionIndex = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + "." + currentQuestion.question;
@@ -49,6 +52,70 @@ function showQuestion() {
 
         button.classList.add("btn");
 
-        answerButton.appendChild(button);
-     });
+        answerButtons.appendChild(button);
+
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+   
+        button.addEventListener("click", selectAnswer);
+    });
 }
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct == "true";
+    if(isCorrect){
+        selectBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButton.children).forEach(button => {
+     
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        } 
+        button.disabled = true;
+    });
+
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `you scored ${score} out of ${questions.length}`;
+    nextButton.innerHTML = "play again";
+    nextButton.style.display = "block";
+}
+
+
+function handleNextButton(){
+
+    currentQuestionIndex++;
+
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else{
+        startQuiz();
+    }
+});
+
+startQuiz();
