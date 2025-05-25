da0d70a38f92591efaa699e9f4f1373546642772
nouribram <nourtesla@yahoo.com>
2025-05-25 04:27:10 +0300

    new

--- BN-A/script.js
+++ BN-A/script.js
@@ -4,6 +4,17 @@ const createBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+
+function updateStorge(){
+
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
 createBtn.addEventListener("click", () => {
 
    let inputBox = document.createElement("p");
@@ -19,3 +30,30 @@ createBtn.addEventListener("click", () => {
    notesContainer.appendChild(inputBox).appendChild(img);
 })
 
+notesContainer.addEventListener("click", function(e)
+{
+    if(e.target.tagName === "IMG")
+     {
+        e.target.parentElement.remove();
+
+        updateStorge();
+     }
+     else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function() {
+              updateStorge();
+            }
+        })
+     }
+
+})
+
+
+document.addEventListener("keydown", event => {
+       
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
