d32d45da615f71a210aacb1d292b603788115bae
nouribram <nourtesla@yahoo.com>
2025-06-15 10:27:03 +0300

    new

---
+++ ddm/script.js
@@ -0,0 +1,8 @@
+/*   
+   let menuToggle = document.querySelector('.menuToggle'); 
+   let navigation = document.querySelector('.navigation');
+       
+   menuToggle.onClick = function() {
+        navigation.classList.toggle('active')
+   } 
+*/
